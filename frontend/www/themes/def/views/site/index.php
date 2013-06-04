1809a746d5fbdb06519c8b0383101a7a468b48e5
llvbest <llvbest@mail.ru>
2013-06-04 09:35:43 +0300

    my commet

---
+++ frontend/www/themes/def/views/site/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * index.php
+ *
+ * @author: antonio ramirez <antonio@clevertech.biz>
+ * Date: 7/22/12
+ * Time: 8:30 PM
+ */
+?>
+Theme My frontend llvbest...
+<?php
+	$this->widget('bootstrap.widgets.TbProgress', array(
+        'type'=>'success', // 'info', 'success' or 'danger'
+        'percent'=>60,
+    ));
+?>
