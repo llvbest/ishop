8015780c0638076209e404a2532185c929a9acb7
llvbest <llvbest@mail.ru>
2013-06-05 07:22:06 +0300

    migration create db

---
+++ console/migrations/createdb.sql
@@ -0,0 +1,7 @@
+﻿CREATE DATABASE `ishop` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+CREATE TABLE IF NOT EXISTS `tbl_migration` (
+  `version` varchar(255) NOT NULL,
+  `apply_time` int(11) DEFAULT NULL,
+  PRIMARY KEY (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
